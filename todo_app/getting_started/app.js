12cec8600f1969114f58bd8665a85661978db4da
theun0524 <theun@postech.ac.kr>
2019-08-02 11:32:16 +0900

    add mongo connection and todo router

--- todo_app/getting_started/app.js
+++ todo_app/getting_started/app.js
@@ -5,8 +5,26 @@ var logger = require('morgan');
 var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
+var todosRouter = require('./routes/todos');
 
 var app = express();
+
+// Import the mongoose module
+var mongoose = require('mongoose');
+
+// Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/todos';
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+// Get the default connection
+var db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -16,5 +34,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/todos', todosRouter);
 
 module.exports = app;
